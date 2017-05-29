3483239a2e2ae89ef1d20ead31937a7ac980104b
balajiachanta <balajiachanta@gmail.com>
2017-05-28 21:50:00 -0500

    Update TriggerComp.java

--- src/main/java/com/bala/drive/notification/TriggerComp.java
+++ src/main/java/com/bala/drive/notification/TriggerComp.java
@@ -20,28 +20,7 @@ public class TriggerComp {
 
 
 
-		if(null != list || list.size() >0){
-			Message message = new Message.Builder().addData("payload", "B64+JSN|eyJjYW1wYWlnbklkIjoiMzY4IiwiZXZlbnROYW1lIjoiUDEwMCIsInRpdGxlIjoiVXBkYXRlIE15IFNwcmludCBNb2JpbGUiLCJhY3Rpb25UeXBlIjoiZXh0ZXJuYWwiLCJhY3Rpb25VcmwiOiJodHRwczovL3BsYXkuZ29vZ2xlLmNvbS9zdG9yZS9hcHBzL2RldGFpbHM/aWQ9Y29tLnNwcmludC5jYXJlIiwiYm9keSI6IkRvd25sb2FkIHRoZSBsYXRlc3QgdmVyc2lvbiBub3cifQ==").build();
-			Sender sender = new Sender("AAAARytYZ5k:APA91bF1KIcQNYxrS7BRu1BCLnEWxyTcCXsaSglYmZPxAmbzaW3GaAAyRTYffm3fktAONWTqxfL1Xju365BtHl27raXcPViYi8xxcTJm8b6z5HTwXe16dL5EdPRrHMH8wa1fxFXeyiZf");
-
-			
-			list.stream().forEach( l -> 
-					{
-						
-						Result result = null;
-						try {
-							result = sender.send(message, l, 1);
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						System.out.println(result);
-
-					}
-					);
-
-			
-		}
+		
 
 		System.out.println("printing s       "+Thread.currentThread().getName());
 		return new AsyncResult<String>(list+Thread.currentThread().getName());
